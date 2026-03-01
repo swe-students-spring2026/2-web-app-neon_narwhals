c13a8b1b186b506a119b2935122375616f29c999
lpn4939-web <lpn4939@nyu.edu>
2026-03-01 00:32:05 -0500

    added a test mongodb

--- grocery.py
+++ grocery.py
@@ -1,35 +1,43 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from pymongo import MongoClient
+import os
 import datetime
+
+
+client = MongoClient(os.getenv("MONGO_URI"))
+
+db1 = client["groceryfood"] #food that groceryList will pull from to add to the list, and also to save the history of what was added to the list each week.
+db2 = client["groceryList"] #current grocery list
 
 grocery_bp = Blueprint('grocery', __name__, template_folder='groceryDisplay')
 
 current_list = []
-grocery_history = [
-    {
-        "week": "10/01/2024",
-        "items": [
-            {"name": "Milk", "date_added": "10/02/2024", "amount": "1 gallon"},
-            {"name": "Eggs", "date_added": "10/03/2024", "amount": "1 dozen"},
-            {"name": "Broccoli", "date_added": "10/03/2024", "amount": "3 heads"},
-        ]
-    },
-    {
-        "week": "10/08/2024",
-        "items": [
-            {"name": "Chicken Breast", "date_added": "10/09/2024", "amount": "2 lbs"},
-            {"name": "Rice", "date_added": "10/09/2024", "amount": "5 lbs"},
-            {"name": "Spinach", "date_added": "10/10/2024", "amount": "2 bags"},
-        ]
-    },
-    {
-        "week": "10/15/2024",
-        "items": [
-            {"name": "Salmon", "date_added": "10/16/2024", "amount": "1.5 lbs"},
-            {"name": "Sweet Potatoes", "date_added": "10/16/2024", "amount": "4"},
-            {"name": "Greek Yogurt", "date_added": "10/17/2024", "amount": "32 oz"},
-        ]
-    }
-]
+# grocery_history = [
+#     {
+#         "week": "10/01/2024",
+#         "items": [
+#             {"name": "Milk", "date_added": "10/02/2024", "amount": "1 gallon"},
+#             {"name": "Eggs", "date_added": "10/03/2024", "amount": "1 dozen"},
+#             {"name": "Broccoli", "date_added": "10/03/2024", "amount": "3 heads"},
+#         ]
+#     },
+#     {
+#         "week": "10/08/2024",
+#         "items": [
+#             {"name": "Chicken Breast", "date_added": "10/09/2024", "amount": "2 lbs"},
+#             {"name": "Rice", "date_added": "10/09/2024", "amount": "5 lbs"},
+#             {"name": "Spinach", "date_added": "10/10/2024", "amount": "2 bags"},
+#         ]
+#     },
+#     {
+#         "week": "10/15/2024",
+#         "items": [
+#             {"name": "Salmon", "date_added": "10/16/2024", "amount": "1.5 lbs"},
+#             {"name": "Sweet Potatoes", "date_added": "10/16/2024", "amount": "4"},
+#             {"name": "Greek Yogurt", "date_added": "10/17/2024", "amount": "32 oz"},
+#         ]
+#     }
+# ]
 
 
 @grocery_bp.route("/grocery-list", methods=["GET", "POST"])
